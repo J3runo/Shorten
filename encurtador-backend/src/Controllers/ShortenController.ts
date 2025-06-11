76b0344f449b7944828b86a499b314e34605f87c
JOSÉ BRUNO OLIVEIRA DA SILVA <jose-bruno_silva@estudante.sesisenai.org.br>
2025-06-10 21:45:23 -0300

    Encurtador finalizado

--- encurtador-backend/src/Controllers/ShortenController.ts
+++ encurtador-backend/src/Controllers/ShortenController.ts
@@ -1,11 +1,12 @@
 import { FastifyInstance, FastifyReply, FastifyRequest } from "fastify";
 import { shortenservice } from "../Services/ShortenService";
+import { request } from "http";
 
 export async function shortenController(app:FastifyInstance) {
     app.post("/shorten", async (request:FastifyRequest, replay:FastifyReply)=>{
         try{
-            const body = request.body as {url:string}
-            const identifier = await shortenservice.register(body.url)
+            const body = request.body as {url:string,shortId:string | null}
+            const identifier = await shortenservice.register(body)
             return identifier
         }catch(error:any){
             return replay.status(404).send({error:"not Found"})
@@ -17,6 +18,16 @@ export async function shortenController(app:FastifyInstance) {
             const query = request.query as {identifier:string}
             const url = await shortenservice.findbyIdentifier(query.identifier)
             return url;
+        }catch(error:any){
+            return replay.status(404).send({error:"not Found"})
+        }
+    })
+
+    app.post("/qr-code",async (request:FastifyRequest, replay:FastifyReply)=>{
+        try{
+            const body = request.body as {url:string}
+            const base64 = await shortenservice.generateQrCode(body)
+            return base64
         }catch(error:any){
             return replay.status(404).send({error:"not Found"})
         }
--- encurtador-backend/src/Services/ShortenService.ts
+++ encurtador-backend/src/Services/ShortenService.ts
@@ -1,16 +1,18 @@
 import { link } from "@prisma/client"
 import { prisma } from "../prisma/client"
 import { customAlphabet } from "nanoid"
-
+import QrCode from 'qrcode'
 
 class ShortenService{
-    public async register(url:string){
+    public async register({url,shortId}:{url:string, shortId:string | null}){
 
-        const shortId = customAlphabet("ABCDEFGHIJKLMNOPQRSTUVXWYZ0123456789", 5)
+        const generateNanoId = customAlphabet("ABCDEFGHIJKLMNOPQRSTUVXWYZ0123456789", 5)
+
+        const custonId = shortId === null ? generateNanoId() : shortId
 
         const link = {
             id:crypto.randomUUID(),
-            shortId:shortId(),
+            shortId:custonId,
             originalUrl: url,
             createdAt:new Date()
         } as link
@@ -27,6 +29,10 @@ class ShortenService{
         }
 
         return {originalUrl: link.originalUrl}
+    }
+    public async generateQrCode({url}:{url:string}){
+        const base64 = await QrCode.toDataURL(url)
+        return {base64:base64}
     }
 }
 
